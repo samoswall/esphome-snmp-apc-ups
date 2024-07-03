bf14116a01432b1b068aee0f91d693572888f842
samoswall <80251553+samoswall@users.noreply.github.com>
2024-07-03 16:23:20 +0300

    Update snmp_component.cpp

--- components/snmp/snmp_component.cpp
+++ components/snmp/snmp_component.cpp
@@ -35,7 +35,7 @@ uint32_t SNMPComponent::get_net_uptime() {
 void SNMPComponent::setup_apc_ups_mib_() {
 
 // firmware_revision
-  snmp_agent_.addDynamicReadOnlyStringHandler(CUSTOM_OID "2.6.0", []() -> std::string { return App.get_text_sensors (); });
+  snmp_agent_.addDynamicReadOnlyStringHandler(CUSTOM_OID "2.6.0", []() -> { return to_string(firmware_revision->get_raw_state()); });
 // last_battery_change_date
   snmp_agent_.addDynamicReadOnlyStringHandler(CUSTOM_OID "2.7.0", []() -> std::string { return App.get_text_sensors (); });
 }
